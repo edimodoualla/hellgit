910dca645f9b82c7916444c92cf64ae57350947a
edimodoualla <edimodoualla@gmail.com>
2017-06-13 11:56:05 -0400

    developer 1 added a line of code

--- HelloGit/src/com/hellogit/HelloGit.java
+++ HelloGit/src/com/hellogit/HelloGit.java
@@ -7,6 +7,8 @@ public class HelloGit {
 		System.out.println("I am learning GIT respository");
 		// This class has been modified
 		System.out.println("I still learning GIT respository");
+		// this comment has been added by developer 1
+		System.out.println("I am developer 1 working in the same files as developer 2.");
 	}
 
 }
