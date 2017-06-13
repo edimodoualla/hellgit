edc810300f5197ca1d22025857132f795c395031
edimodoualla <edimodoualla@gmail.com>
2017-06-13 11:05:17 -0400

    commit HelloGit project

---
+++ HelloGit/src/com/hellogit/HelloGit.java
@@ -0,0 +1,11 @@
+package com.hellogit;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am learning GIT respository");
+
+	}
+
+}
