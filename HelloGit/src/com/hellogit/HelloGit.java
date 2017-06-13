508dbf0d32b85e6fc6522785a3d59d6b153acdb7
edimodoualla <edimodoualla@gmail.com>
2017-06-13 11:16:04 -0400

    commit after change in HelloGit class

--- HelloGit/src/com/hellogit/HelloGit.java
+++ HelloGit/src/com/hellogit/HelloGit.java
@@ -5,7 +5,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am learning GIT respository");
-
+		// This class has been modified
+		System.out.println("I still learning GIT respository");
 	}
 
 }
